b554f399499dc1f6c2b9ab64171cb721875d4a2a
saharoori <saharlaor@gmail.com>
2022-01-07 19:40:59 +0200

    Find image for amazon link anywhere

--- src/components/Items/Item/Item.jsx
+++ src/components/Items/Item/Item.jsx
@@ -5,13 +5,13 @@ import React from "react";
 import "./Item.css";
 
 // Constants
-const PRODUCT_CODE_REGEX = /((?<=\/)[a-zA-Z0-9]+(?=\?))/;
+const PRODUCT_CODE_REGEX =
+  /((?<=https:\/\/www\.amazon\.com\/([a-zA-Z0-9]+\/)+)[a-zA-Z0-9]+(?=\?))/;
 const IMAGE_API_URL = "https://ws-na.amazon-adsystem.com/widgets/q";
 
 function Item({ title, content }) {
   const getImage = () => {
-    const amazonURL = content.split(" ").reverse()[0]; // Last "word" in the content
-    const productCode = amazonURL.match(PRODUCT_CODE_REGEX)[0];
+    const productCode = content.match(PRODUCT_CODE_REGEX);
     const imageURL = `${IMAGE_API_URL}?_encoding=UTF8&MarketPlace=US&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=SL500&ASIN=${productCode}`;
     return imageURL;
   };
