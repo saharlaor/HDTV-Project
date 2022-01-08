1aa68ab8a82daa8b154759cfe3186c33175a66f5
saharoori <saharlaor@gmail.com>
2022-01-08 02:00:04 +0200

    Link URLs using a tags

--- src/components/Items/Item/Item.jsx
+++ src/components/Items/Item/Item.jsx
@@ -1,13 +1,16 @@
 // External
 import React from "react";
+import reactReplace from "react-string-replace";
 
 // Internal
 import "./Item.css";
 
 // Constants
+const IMAGE_API_URL = "https://ws-na.amazon-adsystem.com/widgets/q";
 const PRODUCT_CODE_REGEX =
   /((?<=https:\/\/www\.amazon\.com\/([a-zA-Z0-9]+\/)+)[a-zA-Z0-9]+(?=\?))/;
-const IMAGE_API_URL = "https://ws-na.amazon-adsystem.com/widgets/q";
+const LINK_REGEX = /https?:\/\/www\.[\S]+\.[\S]+\/[\S]*/g;
+const SITE_REGEX = /www\.[^/\s]+\.[^/\s]+/;
 
 function Item({ title, content }) {
   const getImage = () => {
@@ -16,11 +19,30 @@ function Item({ title, content }) {
     return imageURL;
   };
 
+  const getLinkedContent = () => {
+    const links = content.match(LINK_REGEX);
+    const siteTuples = links.map((link) => [link, link.match(SITE_REGEX)[0]]);
+    const cont = reactReplace(content, /(?<=^|[^\n])\n(?=!\n)/g, (_, i) => (
+      <br key={i} />
+    ));
+    const newContent = siteTuples.reduce(
+      (tempContent, [link, site]) =>
+        reactReplace(tempContent, link, (match, i) => (
+          <a key={site + i} href={match}>
+            {site}
+          </a>
+        )),
+      cont
+    );
+    return <div className="Item__content">{newContent}</div>;
+    // console.log(`temp`, <div>{temp}</div>);
+  };
+
   return (
     <div className="Item">
       <img src={getImage()} alt={title} />
-      <h3 className="title">{title}</h3>
-      <p>{content}</p>
+      <h3 className="Item__title">{title}</h3>
+      {getLinkedContent()}
     </div>
   );
 }
