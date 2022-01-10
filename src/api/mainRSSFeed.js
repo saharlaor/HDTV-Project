9a7468ff315c1561f2f9369473d67992d12216fc
saharoori <saharlaor@gmail.com>
2022-01-10 20:28:36 +0200

    More items In row on big screens

--- src/api/mainRSSFeed.js
+++ src/api/mainRSSFeed.js
@@ -12,7 +12,7 @@ async function parse() {
     const { data } = await axios.get(`${PROXY_URL}${API_URL}rss.php`, {
       params: {
         aff: 2,
-        count: 6,
+        count: 12,
         forum: 107,
         image: 2,
       },
