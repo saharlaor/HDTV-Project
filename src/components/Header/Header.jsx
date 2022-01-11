817605154dd71dbf9cafe49d99ac21976eb2029e
saharoori <saharlaor@gmail.com>
2022-01-11 21:42:05 +0200

    Add search and profile icons to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,8 @@
+// External
 import React from "react";
+import { AiOutlineUser, AiOutlineSearch } from "react-icons/ai";
+
+// Internal
 import "./Header.css";
 
 function Header() {
@@ -6,8 +10,11 @@ function Header() {
     <div className="Header">
       <h1 id="main-title">HDTVISRAEL</h1>
       <h2 id="sub-title">אתר הדילים של ישראל</h2>
+      <div className="Header__buttons">
+        <AiOutlineSearch />
+        <AiOutlineUser />
+      </div>
     </div>
-    // TODO: Search at bottom right of Header
   );
 }
 
